458c7a924a7616d0bf0b187ba2dc0b81c04eb8b9
srividya <srividya.ramki@gmail.com>
2018-06-26 14:33:58 -0400

    updated cluster job scripts

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-source activate env-RNAseq
+source activate env-RNASeq
 echo $PWD
 
-snakemake --js $PWD/jobscript.sh\
+snakemake --js $PWD/scripts/jobscript.sh\
     --printshellcmds\
     --restart-times 4\
-    --cluster-config $PWD/cluster.yaml\
+    --cluster-config $PWD/cluster.yml\
     --jobname 'make.{jobid}.{rulename}'\
     --keep-going\
     --stats $PWD/snakemake.stats\
     --timestamp\
     --rerun-incomplete\
     -j 100\
-    --cluster 'qsub -q cmb -l walltime={cluster.time} -l mem={cluster.mem} -l vmem={cluster.mem} -l pmem={cluster.mem} -l nodes=1:ppn={cluster.cores} -o {cluster.logdir} -e {cluster.logdir}'
+    --cluster 'qsub -l walltime={cluster.time} -l mem={cluster.mem} -l vmem={cluster.mem} -l pmem={cluster.mem} -l nodes=1:ppn={cluster.cores} -o {cluster.logdir} -e {cluster.logdir}'
